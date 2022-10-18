f716e1e376f9256ce08d18cc90c7e952b3a2ba51
SanskarSinghal20 <87980166+SanskarSinghal20@users.noreply.github.com>
2022-10-18 22:39:44 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,6 @@
+def my_function(fname) :
+    print(fname + "WORLD")
+
+my_function("hello")
+my_function("hi")
+my_function("bye")
---
+++ test2.py
@@ -0,0 +1,11 @@
+
+
+
+a = 33
+b = 34
+if b>a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+
